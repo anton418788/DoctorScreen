33c5390b4c6fac4b14bcacbc4fb6acb59bb6b87f
Anton Zalagaev <555z.anton@gmail.com>
2023-11-19 09:54:38 +0300

    Initial Commit

---
+++ EffectiveLab2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EffectiveLab2
+//
+//  Created by Admin on 19.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EffectiveLab2/EffectiveLab2App.swift
@@ -0,0 +1,17 @@
+//
+//  EffectiveLab2App.swift
+//  EffectiveLab2
+//
+//  Created by Admin on 19.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct EffectiveLab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
