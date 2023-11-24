b81736ff33d78d89a24a6d3913a528253c962f3d
Anton Zalagaev <555z.anton@gmail.com>
2023-11-24 17:06:03 +0300

    neatData + struct

--- EffectiveLab2/MainScreen/components/NearBlock.swift
+++ EffectiveLab2/MainScreen/components/NearBlock.swift
@@ -14,32 +14,49 @@ struct NearBlock : View {
                 .font(.custom("Poppins-SemiBold", size: 16))
                 .foregroundColor(Color.black)
                 .frame(maxWidth: .infinity, alignment : .leading)
-            DoctorBlock()
+            ForEach (sectionData) { item in
+                DoctorBlock(section: item)
+            }
         }.padding(.top, 16.0)
     }
 }
+struct Section : Identifiable {
+    var id = UUID()
+    var doctor_photo : Image
+    var doctor_name : String
+    var doctor_spec : String
+    var doctor_location : String
+    var doctor_reviews : String
+    var doctor_time : String
+}
+let sectionData = [
+    Section(doctor_photo: Image("2st-doctor"), doctor_name: "Dr. Joseph Brostito", doctor_spec: "Dental Specialist", doctor_location: "1.2 KM", doctor_reviews: "4,8 (120 Reviews)", doctor_time: "11:00 - 12:00 AM"),
+    Section(doctor_photo: Image("image 8"), doctor_name: "Dr. Imran Syahir", doctor_spec: "General Doctor", doctor_location: "1.2 KM", doctor_reviews: "4,8 (120 Reviews)", doctor_time: "11:00 - 12:00 AM"),
+    Section(doctor_photo: Image("2st-doctor"), doctor_name: "Dr. Joseph Brostito", doctor_spec: "Dental Specialist", doctor_location: "1.2 KM", doctor_reviews: "4,8 (120 Reviews)", doctor_time: "11:00 - 12:00 AM"),
+    Section(doctor_photo: Image("image 8"), doctor_name: "Dr. Imran Syahir", doctor_spec: "General Doctor", doctor_location: "1.2 KM", doctor_reviews: "4,8 (120 Reviews)", doctor_time: "11:00 - 12:00 AM")]
 
 struct DoctorBlock : View {
+    var section : Section
     var body : some View {
         VStack {
             HStack {
-                Image("2st-doctor")
+                section.doctor_photo
                     .resizable()
                     .scaledToFill()
                     .frame(width: 57, height: 57, alignment: .top)
                     .background(.white)
                     .clipShape(Circle())
                 VStack (alignment : .leading, spacing: 8){
-                    Text("Dr. Joseph Brostito")
+                    Text(section.doctor_name)
                         .font(.custom("Poppins-Bold", size: 16))
                         .foregroundColor(Color.black)
-                    Text("Dental Specialist")
+                    Text(section.doctor_spec)
                         .font(.custom("Poppins-Regular", size: 14))
                         .foregroundColor(Color.gray)
-                }.padding(.leading, 12    )
+                }.padding(.leading, 12)
                 Spacer()
                 Image("location")
-                Text("1.2 KM")
+                Text(section.doctor_location)
                     .font(.custom("Poppins-Regular", size: 14))
                     .foregroundColor(Color.gray)
 
@@ -50,7 +67,7 @@ struct DoctorBlock : View {
                 .padding(.vertical, 8.0)
             HStack (alignment : .top){
                 Label {
-                    Text("4,8 (120 Reviews)")
+                    Text(section.doctor_reviews)
                         .font(.custom("Poppins-Regular", size: 12))
                         .foregroundColor(Color("reviews-color"))
                 } icon: {
@@ -58,52 +75,7 @@ struct DoctorBlock : View {
                 }
                 Spacer()
                 Label {
-                    Text("11:00 - 12:00 AM")
-                        .font(.custom("Poppins-Regular", size: 12))
-                        .foregroundColor(Color("card-background"))
-                } icon: {
-                    Image("clock-blue")
-                }
-            }
-        }.padding(.horizontal, 16.0).padding(.vertical, 20)
-        VStack {
-            HStack {
-                Image("image 8")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 57, height: 57, alignment: .top)
-                    .background(.white)
-                    .clipShape(Circle())
-                VStack (alignment : .leading, spacing: 8){
-                    Text("Dr. Imran Syahir")
-                        .font(.custom("Poppins-Bold", size: 16))
-                        .foregroundColor(Color.black)
-                    Text("General Doctor")
-                        .font(.custom("Poppins-Regular", size: 14))
-                        .foregroundColor(Color.gray)
-                }.padding(.leading, 12    )
-                Spacer()
-                Image("location")
-                Text("1.2 KM")
-                    .font(.custom("Poppins-Regular", size: 14))
-                    .foregroundColor(Color.gray)
-
-            }
-            Divider()
-                .frame(width: .infinity, height: 1)
-                .overlay(Color("line-color"))
-                .padding(.vertical, 8.0)
-            HStack (alignment : .top){
-                Label {
-                    Text("4,8 (120 Reviews)")
-                        .font(.custom("Poppins-Regular", size: 12))
-                        .foregroundColor(Color("reviews-color"))
-                } icon: {
-                    Image("clock-ye")
-                }
-                Spacer()
-                Label {
-                    Text("11:00 - 12:00 AM")
+                    Text(section.doctor_time)
                         .font(.custom("Poppins-Regular", size: 12))
                         .foregroundColor(Color("card-background"))
                 } icon: {
